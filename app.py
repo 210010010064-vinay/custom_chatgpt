cd1363dc2172ab0a6fff1002f3d8594ae0f9322e
Vinay Rathod <210010010064@iite.ac.in>
2025-09-30 14:32:36 +0530

    Update the Erro

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ from App import create_app
 
 print("Current working directory:", os.getcwd())  
 
-app_1 = create_app()
+app = create_app()
 
 if __name__ == '__main__':
     
     print("Flask app is running on http://127.0.0.1:5000/")
-    app_1.run(host='127.0.0.1', port=5000, debug=True)  
+    app.run(host='127.0.0.1', port=5000, debug=True)  
